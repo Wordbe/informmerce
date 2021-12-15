82f500087bf792cc0cc0d19737b1111b6c22938c
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-12-16 02:56:03 +0900

    fix: getter, setter 추가

--- src/main/java/co/wordbe/informmerce/api/product/dto/ProductResponseDto.java
+++ src/main/java/co/wordbe/informmerce/api/product/dto/ProductResponseDto.java
@@ -1,7 +1,12 @@
 package co.wordbe.informmerce.api.product.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class ProductResponseDto {
     private Long id;
     private String name;
